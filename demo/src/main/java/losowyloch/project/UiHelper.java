88f3900fb3c492710c5ac7e19060a91a4a5bf2ab
SzybkiRabarbar <maksjoni36@gmail.com>
2024-02-05 00:51:50 +0100

    Refactored clearTerminal in UiHelper & Fix issue with empty user input in UiHelper

--- demo/src/main/java/losowyloch/project/UiHelper.java
+++ demo/src/main/java/losowyloch/project/UiHelper.java
@@ -1,27 +1,40 @@
 package losowyloch.project;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UiHelper {
     private Scanner scanner;
+    private int terminalHeight = 100;
     
     public UiHelper() {
         this.scanner = new Scanner(System.in);
     }
 
-    public void clnTer() {
-        System.out.print("\033\143");
+    public void clearTerminal() {
+        try {
+            for (int i = 0; i < terminalHeight; i++) {
+                System.out.println();
+            }
+            new ProcessBuilder("powershell", "/c", "\"Write-Host ('-' * $Host.UI.RawUI.BufferSize.Width)\"").inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
+
 
     public char showAndCollectInput(String[] msgs, char[] validInputs) {
         for (String msg : msgs) {
             System.out.println(msg);
         }
-        char input = scanner.nextLine().charAt(0);
-        this.clnTer();
-        for (char validInput : validInputs) {
-            if (input == validInput) {
-                return input;
+        String line = scanner.nextLine();
+        clearTerminal();
+        if (!line.isEmpty()) {
+            char input = line.charAt(0);
+            for (char validInput : validInputs) {
+                if (input == validInput) {
+                    return input;
+                }
             }
         }
         System.out.println("Złe dane wejściowe! Wpisz znak z pomiedzy nawiasów żeby wybrać opcje!\n");
